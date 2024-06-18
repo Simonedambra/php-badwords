b37adb23f4cf366fd127661ff608552cb305056b
Simonedambra <simonedambra130102@gmail.com>
2024-06-18 14:54:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <form action="result.php" method="GET">
+        <div>
+            <div><label for="my-text">Testo</label></div>
+            <textarea name="my-text" id="my-text"></textarea>
+        </div>
+
+        <hr />
+
+        <div>
+            <div><label for="badword">Parola da censurare</label></div>
+            <input type="text" name="badword" />
+        </div>
+
+        <hr />
+
+        <div>
+            <div><label for="password">Password</label></div>
+            <input type="password" name="password" />
+        </div>
+
+        <hr />
+
+        <button>Invia i dati</button>
+    </form>
+</body>
+
+</html>
